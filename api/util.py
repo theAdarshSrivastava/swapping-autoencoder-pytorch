54fe23731258988f3e1616bed976ce9deec1d796
abhishek <as7122000@gmail.com>
2021-09-25 18:08:11 +0530

    feat: timing util with error and refactor

--- api/util.py
+++ api/util.py
@@ -1,35 +1,73 @@
 import os
+import random
+from dataclasses import dataclass
 from functools import wraps
 from time import time
 from typing import List
-import random
 
 import torch
 from torchvision import transforms
 
 from api.const import KNOWN_IMAGE_FORMATS
 
-tabs = -1
-ON = True
+
+class UtitlState:
+    tabs = -1
+    isON = True
+
+    @classmethod
+    def debug(cls, isON: bool):
+        cls.isON = isON
+        return cls
+
+    @classmethod
+    def add_tab(cls):
+        cls.tabs += 1
+        return cls
+
+    @classmethod
+    def remove_tab(cls):
+        cls.tabs -= 1
+        return cls
 
 
 def timing(f):
+    def print_if_dubug(*args, **kw):
+        """
+        print output if debug is on.
+        """
+        if UtitlState.isON:
+            print(*args, **kw)
+
+    def print_tabs(msg: str):
+        tabs_msg = f" {'  '*UtitlState.tabs} | " if UtitlState.tabs else ""
+        return f"{tabs_msg} {msg}"
+
     @wraps(f)
     def wrap(*args, **kw):
-        global tabs, ON
-        tabs += 1
-        if ON:
-            print(f"{'  '*tabs}|" if tabs else "", f"[ST] func:{f.__name__}")
+        UtitlState.add_tab()
+        args_str = ", ".join([str(ag).replace("\n", " ")[-50:] for ag in args])
+        print_if_dubug(
+            print_tabs(
+                f"[STA] func:{f.__name__}\t \targs:[{args_str}, {kw}]",
+            )
+        )
+
         ts = time()
-        result = f(*args, **kw)
+        try:
+            result = f(*args, **kw)
+        except Exception as e:
+            print_if_dubug(print_tabs(f"[ERR] XxXx"))
+            UtitlState.remove_tab()
+            raise e
         te = time()
-        args_str = [str(ag)[-20:] for ag in args]
-        message = (
-            f"{'  '*tabs}|" if tabs else ""
-        ) + f" [ED] func:{f.__name__}\t\ttook: {round(te-ts, 8)}\targs:[{args_str}, {kw}] sec"
-        tabs -= 1
-        if ON:
-            print(message)
+
+        print_if_dubug(
+            print_tabs(
+                f"[END] func:{f.__name__}\t \ttook: {round(te-ts, 8)}sec",
+            )
+        )
+        UtitlState.remove_tab()
         return result
 
     return wrap
